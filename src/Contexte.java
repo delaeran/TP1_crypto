3c85415cfa6168e251dbf53bb8001a44456a17a5
FofW <fofw@hotmail.fr>
2016-09-22 10:32:01 +0200

    classe contexte

---
+++ src/Contexte.java
@@ -0,0 +1,40 @@
+
+public class Contexte {
+	int N; 					//nombre de mots
+	int mot_taille_min;
+	int mot_taille_max;
+	int N_taille[];
+	int nb_lettre;
+	char lettres;
+
+	
+public void h(String c, byte[] condense ) // hachage
+{
+	
+}
+
+public int h2i(int t, byte[] condense) // hash vers indice
+{
+	return t;
+}
+
+public String i2c(int idx,String c) //index vers mot en clair
+{
+	return c;
+}
+
+public int i2i(int idx) // return l'index 'suivant' à partir d'un index (condense fonction précédentes
+{
+
+	int indexsuiv = 0;
+	return indexsuiv;
+}
+
+public int randIndex()
+{
+	int rand = 0;
+	return rand;
+}
+
+
+}
