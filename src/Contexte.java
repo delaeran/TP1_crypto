df212a13b4e03e3cdcc706ee3470fe3fecfee7f1
Thibaut <thibaut.martin1@etu.univ-savoie.fr>
2016-09-22 10:49:12 +0200

    ajout hash md5

--- src/Contexte.java
+++ src/Contexte.java
@@ -1,3 +1,5 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Contexte {
 	int N; 					//nombre de mots
@@ -8,10 +10,36 @@ public class Contexte {
 	char lettres;
 
 	
-public void h(String c, byte[] condense ) // hachage
+public static String hashmd5(String password, byte[] condense ) // hachage
 {
-	
+	byte[] uniqueKey = password.getBytes();
+    byte[] hash      = null;
+
+    try
+    {
+        hash = MessageDigest.getInstance("MD5").digest(uniqueKey);
+    }
+    catch (NoSuchAlgorithmException e)
+    {
+        throw new Error("No MD5 support in this VM.");
+    }
+
+    StringBuilder hashString = new StringBuilder();
+    for (int i = 0; i < hash.length; i++)
+    {
+        String hex = Integer.toHexString(hash[i]);
+        if (hex.length() == 1)
+        {
+            hashString.append('0');
+            hashString.append(hex.charAt(hex.length() - 1));
+        }
+        else
+            hashString.append(hex.substring(hex.length() - 2));
+        
+    }
+    return hashString.toString();
 }
+
 
 public int h2i(int t, byte[] condense) // hash vers indice
 {
@@ -23,7 +51,7 @@ public String i2c(int idx,String c) //index vers mot en clair
 	return c;
 }
 
-public int i2i(int idx) // return l'index 'suivant' à partir d'un index (condense fonction précédentes
+public int i2i(int idx) // return l'index 'suivant' ï¿½ partir d'un index (condense fonction prï¿½cï¿½dentes
 {
 
 	int indexsuiv = 0;
