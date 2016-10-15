6368752edea457ad28b3f57f24e86d29580e61fb
FofW <fofw@hotmail.fr>
2016-10-15 18:17:42 +0200

    i2c réalisé

--- src/Contexte.java
+++ src/Contexte.java
@@ -9,8 +9,15 @@ public class Contexte {
 	int nb_lettre;
 	char lettres;
 
+	public static void main(String[] args) {
+		String test = hashmd5("aabbo");
+		System.out.println("aabbo");
+		System.out.println(test);
+		
+	}
+
 	
-public static String hashmd5(String password, byte[] condense ) // hachage
+public static String hashmd5(String password ) // hachage
 {
 	byte[] uniqueKey = password.getBytes();
     byte[] hash      = null;
@@ -43,15 +50,38 @@ public static String hashmd5(String password, byte[] condense ) // hachage
 
 public int h2i(int t, byte[] condense) // hash vers indice
 {
-	return t;
+	int indice = 0;
+	//prendre les 8 premiers octets du tableau.
+	//rajouter t 
+	// faire mod N (voir sujet)
+	return indice; // retourne l'indice
+	
 }
 
-public String i2c(int idx,String c) //index vers mot en clair
+public String i2c(int idx,int taille) //index vers mot en clair
 {
-	return c;
+	String result = null;
+	char tableauCaractere[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	//tableau de lettre d'alphabets.
+	int idxactuel = idx;
+	for(int i = 1; i < taille; i++)
+	{
+		if(i ==taille)
+		{
+			int dividx = (int) Math.floor(idxactuel/26);
+			result=tableauCaractere[dividx-1]+result;
+		}
+		else{
+			int modidx = idxactuel%26;
+			result=tableauCaractere[modidx-1]+result;
+			idxactuel = (int) Math.floor(idxactuel/26);
+			
+		}
+	}
+	return result;
 }
 
-public int i2i(int idx) // return l'index 'suivant' � partir d'un index (condense fonction pr�c�dentes
+public int i2i(int idx) // return l'index 'suivant' a partir d'un index (condense fonction precedente)
 {
 
 	int indexsuiv = 0;
