70b8b9b509fc67bb76821f8a3788187d0e10b1e9
FofW <fofw@hotmail.fr>
2016-10-28 19:19:07 +0200

    string to BigInteger done extract info from clepub done

--- src/tp2/testBigInt.java
+++ src/tp2/testBigInt.java
@@ -2,21 +2,36 @@ package tp2;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class testBigInt {
 
 	public static void main(String[] args) {
 		
-		gencle("rien",1024);
-	
+		gencle("rien",32);
+	//	chiffre("rien");
+
+		BigInteger[] test = string_to_ascii(" ABCZ",0);
+		int i = 0;
+		System.out.println(test.length);
+		while (i<test.length)
+		{
+			System.out.println(test[i]);
+			i++;
+		}
+		chiffre("rien");
 	}
 	
 	
@@ -37,7 +52,8 @@ public class testBigInt {
 			q = q.nextProbablePrime();
 			}
 
-		BigInteger n = p.multiply(q); 
+		BigInteger n = p.multiply(q);
+		
 		BigInteger i= BigInteger.valueOf(1);
 		BigInteger b= BigInteger.valueOf(65537);
 		System.out.println(n);
@@ -77,7 +93,7 @@ public class testBigInt {
 			e.printStackTrace();
 		}
 
-		// p,q premier (cachï¿½s)
+		// p,q premier (cache)
 		// n = p x q  (public)
 		// fi(n) (p-1)(q-1)
 		//
@@ -91,15 +107,58 @@ public class testBigInt {
 		// creer fichier de cle nom.priv
 	}
 	
+	
+	
 	public static void chiffre(String nomclepub)
 	{
+		// obtention du n et du b du fichier .publ
+		File file = new File(nomclepub+".publ");
+		if(file.exists())
+		{
+			BufferedReader reader = null;
+			try {
+			    reader = new BufferedReader(new FileReader(file));
+			    String text = reader.readLine();
+		    	String[] clepublique = text.split(" ");
+		    	BigInteger size = new BigInteger(clepublique[0]);
+		        BigInteger cle_n = new BigInteger(clepublique[1]);
+		        BigInteger cle_b = new BigInteger(clepublique[2]);		    	
+			} catch (IOException e) {
+			    e.printStackTrace();
+			}
+		}
+		
+		
 		// chiffre le message sur l'entrï¿½e standard pour le
 		// destinataire de de cle nom.pub
 		// sortie du message sur la sortie standard
 	}
 	
+	
+	public static BigInteger[] string_to_ascii(String text_to_convert,int size)
+	{
+		int stringsize = text_to_convert.length();
+		int actuel = 0;
+		BigInteger[] tab = new BigInteger[stringsize];
+		while(actuel<stringsize)
+		{
+			char c = text_to_convert.charAt(actuel);
+			int ascii_c = (int)c;
+			tab[actuel]= BigInteger.valueOf(ascii_c);
+			actuel++;
+		}
+		return tab;
+	}
+	
+	
+	
+	
+	
+	
 	public static void dechiffre(String nomclepriv)
 	{
+		// clair = crypt^^a mod n
+		
 		// déchiffre avec le fichier nom.priv
 		// sort le message sur la sortie standard.
 		
